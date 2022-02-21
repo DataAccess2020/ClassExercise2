2c8e0ddce712ac1aad15b6e0e357bb1ef6af0677
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-02-21 17:26:40 +0100

    update_2

--- Theorical_settings.R
+++ Theorical_settings.R
@@ -3,3 +3,4 @@
 #The package Rcrawler allows us to do two tasks at the same time: crawling and scraping.
 #Crawling is the process of collecting while browsing different links from a starting point, 
 #scraping is collecting and storing data from the webpages.
+#The situation in the previous situation was different because 
