370f8bc3c52f98d1d40c4fb496d5e5c20eb40301
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-02-21 12:24:56 +0100

    Function for politely download

--- 00_DataImport.R
+++ 00_DataImport.R
@@ -16,3 +16,33 @@ library(httr)
 library(XML)
 library(httr)
 
+polite <- function(from_url, to_html, my_email, my_agent = R.Version()$version.string) {
+  
+  
+  require(httr)
+  
+  # Check that arguments are inputted as expected:
+  stopifnot(is.character(from_url))
+  stopifnot(is.character(to_html))
+  stopifnot(is.character(my_email))
+  
+  # GET politely
+  grillo_scrape <- httr::GET(url = from_url, 
+                             add_headers(
+                               From = my_email, 
+                               `User-Agent` = R.Version()$version.string
+                             )
+  )
+  
+  
+  if (httr::http_status(grillo_scrape)$message == "OK") {
+    bin <- content(grillo_scrape, as = "raw")
+    writeBin(object = bin, con = to_html)
+  } else {
+    cat("Try again")
+  }
+}
+
+polite(from_url = "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/", 
+       to_html = here::here("grillo_plastica.html"), 
+       my_email = "antonio.alaia@studenti.unimi.it")
