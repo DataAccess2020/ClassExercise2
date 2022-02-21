72804caf89e052c19eccc88fda0eb03c08186eb6
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-02-21 17:20:52 +0100

    Theorical settings of a webspider with Rcrawler

---
+++ Theorical_settings.R
@@ -0,0 +1,5 @@
+##Task 5:
+
+#The package Rcrawler allows us to do two tasks at the same time: crawling and scraping.
+#Crawling is the process of collecting while browsing different links from a starting point, 
+#scraping is collecting and storing data from the webpages.
