b8b179dfc86352f15f52317b436eae12a116902e
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-02-21 12:21:56 +0100

    Fixed previous task and managed to find a polite way

--- 00_DataImport.R
+++ 00_DataImport.R
@@ -1,14 +1,18 @@
 #Data Preparation for scraping
 
+
+#First task: checking the robots.txt of beppegrillo.it:
+
+#beppegrillo.it/robots.txt gives us a 404-Not Found page!
+
+#Given that, we can procede to download the website; to download the website page politely
+#we can create a function that download while giving our email and R version details to the webmaster:
+
+
 library(rvest)
 library(tidyverse)
 library(stringr)
 library(httr)
 library(XML)
-
-#First task: checking the robots.txt of beppgrillo.it:
-
-#beppegrillo.it/robots.txt gives us a 404-Not Found page!
-
-#Given that, we can procede to download the website
+library(httr)
 
