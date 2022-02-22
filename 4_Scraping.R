dd70dc9c8464e01a81c2d20428cc0019e72bde85
maria-ascolese <mariaascoleseasdnt@gmail.com>
2022-02-22 14:58:55 +0100

    Test to see if the code works for one url

--- 4_Scraping.R
+++ 4_Scraping.R
@@ -7,3 +7,17 @@ library(stringr)
 url <- c("https://beppegrillo.it/category/archivio/2016/",
          str_c("https://beppegrillo.it/category/archivio/2016/page/", 2:47))
 #The first url was different than the others, as only 2:46 contained "/page/n"
+
+
+#In order to do point b, I need to get every url for the 10 posts in each page
+#I'm gonna do it for the first page, to get the strategy
+
+prova <- "https://beppegrillo.it/category/archivio/2016/"
+
+
+library(rvest)
+
+link <- read_html(prova) %>%
+  html_elements(css = ".td_module_10 .td-module-title")
+
+#If I print(link), I see all 10 links for the 10 posts in the first page. The code works
